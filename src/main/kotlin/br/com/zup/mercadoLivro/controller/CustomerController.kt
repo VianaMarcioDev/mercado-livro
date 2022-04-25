3dae1880404481fb3e576690199c96281e32f05a
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-04-24 21:47:06 -0300

    Criar método Post na classe CustomerController

--- src/main/kotlin/br/com/zup/mercadoLivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/zup/mercadoLivro/controller/CustomerController.kt
@@ -2,6 +2,8 @@ package br.com.zup.mercadoLivro.controller
 
 import br.com.zup.mercadoLivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +14,8 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Márcio", "marcio@email.com")
+    }@PostMapping
+    fun createCustomer(@RequestBody customer: CustomerModel) {
+        println(customer)
     }
 }
---
+++ src/main/kotlin/br/com/zup/mercadoLivro/controller/request/PostCustomerRequest.kt
@@ -0,0 +1,4 @@
+package br.com.zup.mercadoLivro.controller.request
+
+data class PostCustomerRequest {
+}
