771f432268a1d598af770083389878d060069c73
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-04-24 21:02:02 -0300

    Primeiro commit. Criar projeto mercado-livro. Criar classe CustomerController

---
+++ src/main/kotlin/br/com/zup/mercadoLivro/controller/CustomerController.kt
@@ -0,0 +1,15 @@
+package br.com.zup.mercadoLivro.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello World"
+    }
+}
---
+++ src/test/kotlin/br/com/zup/mercadoLivro/MercadoLivroApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.zup.mercadoLivro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
