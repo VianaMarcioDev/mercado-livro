788462e2189eff51e13a9af697c30acce2600eae
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-04-24 22:06:14 -0300

    Alterar HttpStatus response

--- src/main/kotlin/br/com/zup/mercadoLivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/zup/mercadoLivro/controller/CustomerController.kt
@@ -2,11 +2,14 @@ package br.com.zup.mercadoLivro.controller
 
 import br.com.zup.mercadoLivro.controller.request.PostCustomerRequest
 import br.com.zup.mercadoLivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.annotation.HttpMethodConstraint
 
 @RestController
 @RequestMapping("customers")
@@ -15,7 +18,10 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Márcio", "marcio@email.com")
-    }@PostMapping
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createCustomer(@RequestBody customer: PostCustomerRequest) {
         println(customer)
     }
